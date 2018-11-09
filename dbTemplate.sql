5bc2dce57cf672c59579dbdd5b82e1450b4e1ad3
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2018-11-09 00:34:05 -0200

    CI + db

--- dbTemplate.sql
+++ dbTemplate.sql
@@ -1,1 +1,33 @@
 --CREATE DATABASE sicfdb;
+
+CREATE TABLE passivos(
+    codPsv SERIAL NOT NULL PRIMARY KEY,
+    nome VARCHAR NOT NULL,
+    valor FLOAT NOT NULL,
+    data DATE,
+    multa FLOAT,
+    juros FLOAT
+);
+
+CREATE TABLE ativos(
+    codAtv SERIAL NOT NULL PRIMARY KEY,
+    nome VARCHAR NOT NULL,
+    valor FLOAT NOT NULL,
+    data DATE
+);
+
+CREATE TABLE usuario(
+    codUsu SERIAL NOT NULL PRIMARY KEY,
+    nome VARCHAR NOT NULL,
+    login VARCHAR NOT NULL,
+    senha VARCHAR(32) NOT NULL
+    --email VARCHAR NOT NULL,
+    --celular VARCHAR NOT NULL
+);
+
+CREATE TABLE caixa(
+    codConta SERIAL NOT NULL PRIMARY KEY,
+    competencia INT NOT NULL,
+    saldo FLOAT NOT NULL,
+    codUsu INT REFERENCES usuario(codUsu)
+);
