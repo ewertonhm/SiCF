20358ca894be136ccb6eba2028b67bf35be7adaa
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2018-11-09 21:07:13 -0200

    .

---
+++ application/views/header.php
@@ -0,0 +1,15 @@
+<?php $this->load->view('head'); ?>
+<header id="header">
+    <h1>Adam Jensen</h1>
+    <p>Security Chief &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never asked for this</p>
+    <nav>
+        <ul>
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+            <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+	</ul>
+    </nav>
+</header>
+<?php $this->load->view('footer'); ?>
